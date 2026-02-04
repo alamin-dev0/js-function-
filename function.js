bfbbc6c65418e3b61cb150e301f69028673a5573
Al Amin <alamin.dev01@gmail.com>
2026-02-04 20:43:31 +0600

    added some code

---
+++ function.js
@@ -0,0 +1,29 @@
+const number = 45;
+
+function fanOffKor() {
+    console.log('Bosa theke othe deara')
+    console.log('walk toward the withch')
+    console.log('click the switch')
+}
+
+
+// call the function 
+
+fanOffKor();
+// function name (){}
+//
+function brushTeet() {
+    console.log('pick up the brush ')
+    console.log('add past')
+}
+
+// call the function 
+
+brushTeet();
+
+console.log('------------------')
+console.log('------------------')
+console.log('------------------')
+console.log('------------------')
+console.log('------------------')
+brushTeet()
---
+++ perametar.js
@@ -0,0 +1,38 @@
+function vaatkhao() {
+    console.log('hand wash kore aso')
+    console.log('boso')
+    console.log('plate e khabar dhabar nao ')
+    console.log('gopat gopat kore khao ')
+}
+
+// vaatkhao()
+
+// 3--> 3*3 = 9
+// 4--> 4*4 = 16
+// 5 --> 5*5 = 25
+// 11 --> 11*11= 121
+
+function square(number) {
+    const borgo = number * number;
+    console.log('square of the given number is ', borgo)
+}
+square(10)
+
+console.log('-------------------')
+square(20)
+console.log('-------------------')
+square(500)
+
+function add(num1, num2) {
+    const sum = num1 + num2
+    console.log(sum)
+}
+add(5, 22)
+add(500, 500)
+
+function addAll(a, b, c, d, e) {
+    const total = a + b + c + d + e
+    console.log(total)
+
+}
+addAll(3, 5, 6, 7, 9)
