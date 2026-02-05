31fabe998475e540aab3a3858eee74358fd096a6
Al Amin <alamin.dev01@gmail.com>
2026-02-05 23:45:44 +0600

    added some code

--- task.js
+++ task.js
@@ -22,4 +22,30 @@ function OddEven(number) {
 const theNumber = 100;
 
 const theResult = OddEven(theNumber)
-console.log(theResult)
+// console.log(theResult)
+
+
+
+
+function make_avg(avgOfNum) {
+    let sum = 0;
+    for (const num of avgOfNum) {
+        sum = sum + num;
+    }
+    // console.log(sum)
+    let avg_num = sum / avgOfNum.length
+    return avg_num;
+}
+
+
+const avg_array = [2, 4, 6, 8]
+
+const avg = make_avg(avg_array)
+// console.log(avg)
+// console.log(make_avg([44, 55, 66, 77, 88, 999, 11, 222, 33,412431234,123412341,123412341,132412341,]))
+
+
+function getMenu() {
+ 
+    console.log("Burger, Pizza, Pasta");
+}
