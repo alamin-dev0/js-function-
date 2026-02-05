80625ab92f87d40213feb609631bb593991294e1
alamin-dev0 <alamin.dev01@gmail.com>
2026-02-05 19:01:15 +0600

    added some code

---
+++ task.js
@@ -0,0 +1,25 @@
+function multiplyNumber(num1, num2, num3, num4) {
+    const multiply = num1 * num2 * num3 * num4;
+    return multiply;
+}
+
+const multiplyValue = multiplyNumber(4, 3, 6, 5)
+// console.log(multiplyValue)
+
+
+
+
+function OddEven(number) {
+    if (number % 2 === 1) {
+        number = number * 2
+    }
+    else if (number % 2 === 0) {
+        number = number % 2;
+    }
+    return number;
+}
+
+const theNumber = 100;
+
+const theResult = OddEven(theNumber)
+console.log(theResult)
